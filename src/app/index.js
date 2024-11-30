1547f6eaaf7643a717f9461055edb366d1bfbc22
lívia <162451674+liviapollyana@users.noreply.github.com>
2024-11-30 17:51:17 +0000

    [feat] Expo-font with Context

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Minha primeira janela</Text>
+      <Text style={styles.title}>Minha primeira janela</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontFamily: "light",
+    fontSize: 20,
   },
 });
