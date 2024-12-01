f9ac6322c2fbd9bd42cadea1c3b276ff2f3f963e
lívia <162451674+liviapollyana@users.noreply.github.com>
2024-12-01 20:15:29 +0000

    aula #7

--- src/app/index.js
+++ src/app/index.js
@@ -1,37 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { BackHandler, Button, StyleSheet, Text, View } from 'react-native';
+import { BackHandler, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { useAuth } from '../hooks/Auth';
 import { router } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 export default function App() {
   const { signIn, signOut } = useAuth();
+  const [email, setEmail] = useState("super@email.com");
+  const [password, setPassword] = useState("A123456a!");
+  const [passwordVisbility, setPasswordVisibility] = useState(false);
+
+  const tooglePasswordVisibility = () => {
+    setPasswordVisibility(!passwordVisbility);
+  };
 
   const handleEntrarSuper = async () => {
     try {
-      await signIn({ email: "super@email.com", password: "A123456a!" });
+      await signIn({ email, password});
       router.replace("/");
     } catch (error) {
+      Alert.alert("Erro", error.message);
       console.log(error);
     }
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Minha primeira janela</Text>
+      <Text style={styles.title}>o</Text>
+      <View style={styles.inputbox}>
+        <Ionicons name="mail-open-outline" size={20} color="black" />
+        <TextInput 
+          style={styles.emailinput}
+          placeholder="E-mail" 
+          value={email} 
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.inputbox}>
+        <Ionicons name="lock-closed-outline" size={20} color="black" />
+        <TextInput 
+          style={styles.emailinput}
+          placeholder="Senha" 
+          value={password} 
+          onChangeText={setPassword}
+          secureTextEntry={passwordVisbility}
+        />
+        <Ionicons
+          name={passwordVisbility? "eye-off-outline" : "eye-outline"}
+          size={20}
+          color="black"
+          onPress={tooglePasswordVisibility}
+        />
+      </View>
+
       <Button 
-        title="Signin Super" 
-        onPress={handleEntrarSuper}
-      />
-      <Button 
-        title="Signin Adm" 
-        onPress={()=> 
-          signIn({email: "adm@email.com", password: "Adm123!"})
-        }
-      />
-      <Button 
-        title="Signin User" 
-        onPress={()=> 
-          signIn({email: "user@email.com", password: "User123!"})
-        }
+        style={styles.button}
+        title="Entrar" 
+        onPress={handleEntrarSuper} 
       />
       <Button title="Sobre" onPress={()=>router.push("/about")} />
         <Button
@@ -54,5 +79,20 @@ const styles = StyleSheet.create({
   title: {
     fontFamily: "light",
     fontSize: 20,
+  },
+  inputbox: {
+    flexDirection: "row",
+    gap: 10,
+    marginHorizontal: 40,
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  emailinput: {
+    flex: 1,
+    fontFamily: "regular",
+    fontSize: 20,
+  },
+  button: {
+    width: "100%",
   },
 });
